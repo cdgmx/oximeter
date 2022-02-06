0cb5ab806cf471cbd4aaff83a75f8285faee39a5
cdgmx <iamcdgmx@gmail.com>
2022-02-06 13:49:10 +0800

    Update App.js
    remove template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import SensorReading from './components/SensorReading';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <SensorReading/>
+          </header>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
